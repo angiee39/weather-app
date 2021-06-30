3107b2d58994202ba7527dbbdda2dec273dfb0fb
angiee39 <anjalarox@gmail.com>
2021-06-30 13:13:42 +0530

    Task completed, need better UI.

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 const content = document.getElementById('content')
+let fahrenheit = true
 
-const form = document.createElement('form')
-const i 
+let city = ""
+let temp = ""
+let weather = ""
 
-
-
-
-
-async function getWeatherInfo() {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=ab427a4ef316df912aa8a386e9ce3e86')
+async function getWeatherInfo(location) {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ab427a4ef316df912aa8a386e9ce3e86`)
   const weatherData = await response.json()
-  // content.innerHTML = weatherData
+  
   console.log(weatherData.name)
   console.log(weatherData.main.temp)
   console.log(weatherData.weather[0].main)
+  
+  const absTemp = weatherData.main.temp
+  city = weatherData.name
+  temp = tempConvert(absTemp).toFixed(2)
+  weather = weatherData.weather[0].main
+  renderInfo()
 }
-getWeatherInfo()
+
+function tempConvert(temp) {
+  const metric = document.getElementById('metric').value
+  let newTemp = 0
+  if (metric === "c") {
+    newTemp = temp - 273.15;
+  } else {
+    newTemp = (temp - 273.15) * 9/5 + 32;
+  }
+  return newTemp
+}
+
+const form = document.getElementById('form')
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const location = document.getElementById('location').value
+  getWeatherInfo(location)
+})
+
+function renderInfo() {
+  const cityLabel = document.getElementById('city')
+  cityLabel.textContent = `City: ${city}`
+  const tempLabel = document.getElementById('temp')
+  tempLabel.textContent = `Temperature: ${temp}`
+  const weatherLabel = document.getElementById('weather')
+  weatherLabel.textContent = `Weather: ${weather}`
+}
+
+
+
