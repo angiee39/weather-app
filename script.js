8770a526f41e108a207e0bac4598a58935b39b80
angiee39 <anjalarox@gmail.com>
2021-07-02 12:57:02 +0530

    Added misc styles.

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ let fahrenheit = true
 let city = ""
 let temp = ""
 let weather = ""
+let sign = ""
+let description = ""
 
 async function getWeatherInfo(location) {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=ab427a4ef316df912aa8a386e9ce3e86`)
   const weatherData = await response.json()
   
-  console.log(weatherData.name)
-  console.log(weatherData.main.temp)
-  console.log(weatherData.weather[0].main)
-  
   const absTemp = weatherData.main.temp
   city = weatherData.name
-  temp = tempConvert(absTemp).toFixed(2)
+  temp = tempConvert(absTemp).toFixed(1)
   weather = weatherData.weather[0].main
+  description = weatherData.weather[0].description
   renderInfo()
 }
 
@@ -25,8 +24,10 @@ function tempConvert(temp) {
   let newTemp = 0
   if (metric === "c") {
     newTemp = temp - 273.15;
+    sign = "C"
   } else {
     newTemp = (temp - 273.15) * 9/5 + 32;
+    sign = "F"
   }
   return newTemp
 }
@@ -40,12 +41,28 @@ form.addEventListener('submit', e => {
 
 function renderInfo() {
   const cityLabel = document.getElementById('city')
-  cityLabel.textContent = `City: ${city}`
+  cityLabel.textContent = city
   const tempLabel = document.getElementById('temp')
-  tempLabel.textContent = `Temperature: ${temp}`
+  tempLabel.textContent = temp + " " + sign
   const weatherLabel = document.getElementById('weather')
-  weatherLabel.textContent = `Weather: ${weather}`
+  weatherLabel.textContent = description
+  const card = document.getElementById('card')
+  card.classList.remove('hide')
+  weatherIcon(weather)
 }
 
+const weatherIcon = (condition) => {
+  const div = document.getElementById('weather-icon')
+  div.innerHTML = ""
+  const i = document.createElement('i')
 
+  if (condition === "Clouds") {
+    i.setAttribute('class', 'fas fa-cloud-sun fa-4x')
+  } else if (condition === "Clear") {
+    i.setAttribute('class', 'fas fa-sun fa-4x')
+  } else if (condition === "Rain") {
+    i.setAttribute('class', 'fas fa-cloud-rain fa-4x')
+  }
+  div.appendChild(i)
+}
 
